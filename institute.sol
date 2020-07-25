5bb3860849aacec2f999fc4ab22e9987c79420b8
Shavindu Senadheera <42552426+shavinsenadheera@users.noreply.github.com>
2020-07-25 19:39:00 +0530

    version 0.1

--- institute.sol
+++ institute.sol
@@ -4,16 +4,30 @@ contract Institute
 {
     uint256 id;
     string name;
+    address owner;
     
+    event instructor( uint _id, string _name );
+    
+    constructor() public
+    {
+        owner = msg.sender;
+    }
+    
+    modifier isOwner
+    {
+        require(owner==msg.sender, "You are not owner..! Do not cheat!");
+        _;
+    }
     
     function getInstructor() public view returns(uint, string memory)
     {
         return (id, name);
     }
     
-    function addInstructor(uint256 _id, string memory _name) public
+    function addInstructor(uint256 _id, string memory _name) public isOwner
     {
         id = _id;
         name = _name;
+        emit instructor(id, name);
     }
 }

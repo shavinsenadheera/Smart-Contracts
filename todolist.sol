ae6d7fb1cd74658e4a8a1c54db0598c7f8356cf0
Shavindu Senadheera <42552426+shavinsenadheera@users.noreply.github.com>
2020-08-07 20:44:49 +0530

    version 0.1

---
+++ todolist.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.17;
+
+contract Transfer
+{
+    mapping(uint=>List) list;
+    event listone(uint, string);
+    uint256 count =0;
+    struct List
+    {
+        uint id;
+        string title;
+    }
+    
+    address owner;
+    
+    constructor() public
+    {
+        owner = msg.sender;
+    }
+    
+    modifier isowner
+    {
+        require(owner==msg.sender, "You are not owner!");
+        _;
+    }
+    
+    function store(string memory _name) public isowner
+    {
+        list[count+100] = List(count+1, _name);
+        emit listone(count, _name);
+        count++;
+    }
+    
+    function returncount() public view returns(uint)
+    {
+        return count;
+    }
+    
+    function getList(uint _index) public view returns(uint, string memory)
+    {
+        return (list[_index].id, list[_index].title);
+    }
+    
+}

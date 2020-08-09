4e73f891b7fdbcbe8683eb409e0f343660d0376a
Shavindu Senadheera <42552426+shavinsenadheera@users.noreply.github.com>
2020-08-10 03:15:33 +0530

    version 0.11

--- todolist.sol
+++ todolist.sol
@@ -4,6 +4,7 @@ contract Transfer
 {
     mapping(uint=>List) list;
     event listone(uint, string);
+    event trns(uint, address);
     uint256 count =0;
     struct List
     {
@@ -39,6 +40,11 @@ contract Transfer
     function getList(uint _index) public view returns(uint, string memory)
     {
         return (list[_index].id, list[_index].title);
+    }
+    
+    function deleteItem(uint _index) public isowner
+    {
+        delete list[_index];
     }
     
 }
